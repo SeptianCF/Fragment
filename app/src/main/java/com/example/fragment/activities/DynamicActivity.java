a419c40b1fb4d11c7d36df4be087c1ed4006d99e
Septian Caesar Floresko <septian9caesar@gmail.com>
2019-09-26 16:30:40 +0700

    update Latian

--- app/src/main/java/com/example/fragment/activities/DynamicActivity.java
+++ app/src/main/java/com/example/fragment/activities/DynamicActivity.java
@@ -33,10 +33,13 @@ public class DynamicActivity extends AppCompatActivity {
 //            fragmentTransaction.addToBackStack(null);
 //            fragmentTransaction.commit();
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
-        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new RedFragment(),"RED_FRAGMENT");
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+
+       if(getSupportFragmentManager().findFragmentByTag("RED_FRAGMENT")==null) {
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+            fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new RedFragment(),"RED_FRAGMENT");
+//            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+       }
     }
 
     public void handlerClickLoadBlueFragment(View view) {
@@ -53,10 +56,12 @@ public class DynamicActivity extends AppCompatActivity {
 //            fragmentTransaction.addToBackStack(null);
 //            fragmentTransaction.commit();
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
-        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new BlueFragment(),"BLUE_FRAGMENT");
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
+        if(getSupportFragmentManager().findFragmentByTag("BLUE_FRAGMENT")==null) {
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+            fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new BlueFragment(),"BLUE_FRAGMENT");
+//            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
 
     }
 }
